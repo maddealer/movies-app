fb79177b9584ee31fb88f0f9f9bcad933b7a0564
maddealer <forento@gmail.com>
2020-04-11 18:39:49 +0300

    initial commit"

---
+++ server/db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cinema", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(e => {
+    console.error("Connection error", e.message);
+  });
+
+const db = mongoose.connection;
+
+module.exports = db;
